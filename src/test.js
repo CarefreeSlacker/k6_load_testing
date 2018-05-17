659b38fbf25f4eb9e296f7fb60398b7359b14dbc
a.kurdyukov <a.kurdyukov@cti.ru>
2018-05-17 23:08:47 +0300

    First connection has been implemented #making_full_featured

--- src/test.js
+++ src/test.js
@@ -1,16 +1,17 @@
 import http from "k6/http"
 import ws from "k6/ws";
+import { check } from "k6";
+// import { Channel } from './phoenix.js'
 
 const customerUrl = 'http://localhost:4000/api/customers'
 const regularUrl = 'http://localhost:4000/api'
-let authorizationHeader = '';
 
 const sendCustomerRequest = function(payload) {
     const request = http.post(customerUrl, JSON.stringify(payload), { headers: {'Content-Type': 'application/json'} })
     return JSON.parse(request.body)
 }
 
-const sendRegularRequest = function(payload) {
+const sendRegularRequest = function(payload, authorizationHeader) {
     const requestHeaders = Object.assign(authorizationHeader, {'Content-Type': 'application/json'})
     const request = http.post(regularUrl, JSON.stringify(payload), { headers: requestHeaders })
     return JSON.parse(request.body)
@@ -60,7 +61,7 @@ const signIn = function(authId, token) {
     return result.data.tokenSignIn
 }
 
-const getRequestTypes = function() {
+const getRequestTypes = function(requesHeaders) {
     const result = sendRegularRequest({
         query: "query requestTypes {\
                          requestTypes {\
@@ -73,11 +74,12 @@ const getRequestTypes = function() {
                        }\
                        ",
         operationName: 'requestTypes'
-    })
+    },
+    requesHeaders)
     return result.data.requestTypes
 }
 
-const initActivity = function(requestTypeId) {
+const initActivity = function(requestTypeId, requesHeaders) {
     const result = sendRegularRequest({
         query: "mutation createActivity($requestTypeId: String!) {\
                          activityByRequestType(requestTypeId: $requestTypeId) {\
@@ -89,17 +91,101 @@ const initActivity = function(requestTypeId) {
             requestTypeId: parseInt(requestTypeId)
         },
         operationName: 'createActivity'
-    })
+    },
+    requesHeaders)
     return result.data.activityByRequestType.id
 }
+
+
+// WS client
+
+const wsUrl = 'ws://localhost:4000/socket/websocket';
+
+const wsBlock = function(guardianToken, customerName, activityId) {
+    const wsParams = {
+        'guardian_token': guardianToken,
+        'username': customerName,
+        'vsn': "2.0.0"
+    }
+    console.log(`wsBlock params: ${JSON.stringify(wsParams)}`)
+    const socketUrl = `${wsUrl}?${objectToParams(wsParams)}`
+    const result = ws.connect(socketUrl, { params: wsParams }, function(socket) {
+        socket.on('open', function(response) {
+            console.log(`open response: ${JSON.stringify(response)}`);
+        });
+        socket.on('phx_reply', function(response) {
+            console.log(`phx_reply response: ${JSON.stringify(response)}`);
+        });
+
+        socket.on('presense_state', function(data) {
+            console.log("presense_state: ", JSON.stringify(data));
+        });
+
+        socket.on('presense_diff', function(data) {
+            console.log("presense_diff: ", JSON.stringify(data));
+        });
+
+        socket.on('phx_reply', function(data) {
+            console.log("phx_reply: ", JSON.stringify(data));
+        });
+
+        socket.on('close', function() {
+            console.log('disconnected');
+        });
+
+        socket.send(
+            JSON.stringify(
+                [1, 2, `room:${activityId}`, 'phx_join', {'guardian_token': guardianToken}]
+            )
+        )
+
+        socket.setTimeout(function () {
+            console.log('4 seconds passed, closing the socket');
+            socket.close();
+        }, 4000);
+    });
+    console.log(`socket is ready: ${JSON.stringify(result)}`)
+    check(result, { "status is 101": (r) => r && r.status === 101 });
+    return ws;
+}
+
+const objectToParams = function(obj) {
+    var str = "";
+    for (var key in obj) {
+        if (str != "") {
+            str += "&";
+        }
+        str += key + "=" + encodeURIComponent(obj[key]);
+    }
+    return str
+}
+
 
 export default function() {
     const authId = initAuth('89997777777')
     const { customer, token } = signIn(authId, '12345678')
-    console.log(`customer: ${JSON.stringify(customer)},\n token: ${JSON.stringify(token)}`)
-    authorizationHeader = { 'authorization': `Bearer ${token}` }
-    const requestTypes = getRequestTypes()
+    const authorizationHeader = { 'authorization': `Bearer ${token}` }
+    const requestTypes = getRequestTypes(authorizationHeader)
     const randomRequestType = requestTypes[Math.floor(Math.random() * requestTypes.length)]
-    const activityId = initActivity(randomRequestType.id)
-    console.log(`activityId: ${activityId}`)
+    const activityId = initActivity(randomRequestType.id, authorizationHeader)
+    wsBlock(token, customer.firstName, activityId)
+    console.log(`ws: ${JSON.stringify(ws)}`)
+    return true
 }
+
+// "1"
+// 1
+// :
+// "1"
+// 2
+// :
+// "room:7eff2071-d82c-4a59-9554-a297e382e1bf"
+// 3
+// :
+// "phx_join"
+// 4
+// :
+// {,…}
+// guardian_token
+//     :
+//     "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJDdXN0b21lcjoxIiwiZXhwIjoxNTI4ODEyMzg5LCJpYXQiOjE1MjYyMjAzODksImlzcyI6IkN0aU9tbmkiLCJqdGkiOiJjYzg4YThlMy0zZjY5LTQxNDgtOWE3Zi1kOWQ0MWIzNGNkY2MiLCJwZW0iOnt9LCJzdWIiOiJDdXN0b21lcjoxIiwidHlwIjoiYWNjZXNzIn0.k4vSDUiEjaYiPr-iPPBfi3Wsq6TkXkxTscGztP3-C-XB7WiVtNMTeLHKrAUAnehP4M72e6HuS6we7Z4ux4M
